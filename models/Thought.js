0bda536771536fcc2b3c0cd38901b8b951f452ee
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-03-13 14:02:44 -0500

    add thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,31 @@
 const { Schema, model, Types } = require('mongoose');
-const moment = require ('moment');
+const moment = require('moment');
 
-
-
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => moment(createdAtVal).format('MM/DD/YY [at] HH:MM'),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 module.exports = Thought;
